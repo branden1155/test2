74ec1c22e7ff4a4caaee78f981c64cc6aa8a3929
MurphyBranden-FS-1 <93297203+MurphyBranden-FS-1@users.noreply.github.com>
2022-07-03 05:01:07 -0400

    New test for the string module -- all pass

---
+++ string/string.js
@@ -0,0 +1,11 @@
+// Uppercase function for TEST
+const uppercase = (str) => str.toUpperCase();
+
+// Lowercase function for TEST
+const lowercase = (str) => str.toLowerCase();
+
+//exporting the module
+module.exports = {
+    uppercase,
+    lowercase
+}
---
+++ string/string.test.js
@@ -0,0 +1,10 @@
+const {uppercase, lowercase} = require("./string")
+describe("Testing the string module", () =>{
+    test("Should uppercase a string input",()=>{
+        expect(uppercase("bob")).toBe("BOB");
+    })
+
+    test("Should lowercase a string input", () => {
+        expect(lowercase("FULL SaiL")).toBe("full sail");
+    })
+});
