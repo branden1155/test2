d08b295939c429641d4486b75a10a2dcf7730fba
MurphyBranden-FS-1 <93297203+MurphyBranden-FS-1@users.noreply.github.com>
2022-07-03 02:59:07 -0400

    Updated server.js Added Comment

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use((req, res, next) => {
     error.status = 404;
     next(error);
 });
-
+// middle ware to sen error nicely
 app.use((error, req, res, next) => {
     res.status(error.status || 500).json({
         error: {
